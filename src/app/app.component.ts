67d8106cec0047ef48ff2fd189f2a80dfb5583d5
Leonardo <regalado.zuniga.leonardo.daniel@gmail.com>
2021-11-15 15:29:44 -0600

    conexion a modulo de iniciar sesion y login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  public linkCss: Element | null;
-
-  constructor() {
-    this.linkCss = document.querySelector('#style-global');
-
-    console.log(this.linkCss?.getAttribute('href'));
-  }
-
 }
--- src/app/modules/home/components/home/home.component.ts
+++ src/app/modules/home/components/home/home.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public linkCss: Element | null;
+
+  constructor() {
+    this.linkCss = document.querySelector('#style-global');
+    this.linkCss?.setAttribute('href', './assets/styles/home.css');
+  }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    this.linkCss?.setAttribute('href', './assets/styles/default.css')
   }
 
 }
