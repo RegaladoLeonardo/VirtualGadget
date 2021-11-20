5a08157dcb7830eaadb3844d04bfd96330e16dc8
Nuñez Manrique Cuauhtémoc Emmanuel <76032890+NunezManriqueCuauhtemocEmmanuel@users.noreply.github.com>
2021-11-19 19:15:37 -0600

    registro beta

--- src/app/modules/auth/components/singup/singup.component.ts
+++ src/app/modules/auth/components/singup/singup.component.ts
@@ -1,16 +1,20 @@
-import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-singup',
   templateUrl: './singup.component.html',
   styleUrls: ['./singup.component.css']
 })
-export class SingupComponent implements OnInit{
+export class SingupComponent implements OnDestroy{
+  public linkCss: Element | null;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.linkCss = document.querySelector('#style-global');
+    this.linkCss?.setAttribute('href', './assets/styles/signup.css');
   }
 
-
+  ngOnDestroy(): void {
+    this.linkCss?.setAttribute('href', './assets/styles/default.css');
+    console.log('Soy el ondestroy');
+  }
 }
