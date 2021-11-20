c11b82c9631526bf38c4ef30ec48ea8f3203e331
Leonardo <regalado.zuniga.leonardo.daniel@gmail.com>
2021-11-20 05:14:58 -0600

    form group in singup

--- src/app/modules/auth/components/singup/singup.component.ts
+++ src/app/modules/auth/components/singup/singup.component.ts
@@ -1,15 +1,34 @@
 import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
-
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { Store } from 'src/app/core/store/store';
 @Component({
   selector: 'app-singup',
   templateUrl: './singup.component.html',
   styleUrls: ['./singup.component.css']
 })
-export class SingupComponent implements OnInit{
+export class SingupComponent {
 
-  constructor() { }
+  public storeSub: Subscription;
+  public state: any;
+  public form!: FormGroup;
 
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private store: Store
+  ) {
+    this.makeForm();
+    this.storeSub = this.store.getObservable.subscribe((store) => {
+      this.state = store;
+  })
+  }
+
+
+  public makeForm(): void {
+    this.form = this.fb.group({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
+    })
   }
 
 
